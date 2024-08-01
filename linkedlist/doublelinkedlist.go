860f5a638f20963c018db4766287b8ab073fa033
John F <john@johnscode.com>
2024-08-01 12:52:35 -0500

    refactor double list to be interface, update impl

--- linkedlist/doublelinkedlist.go
+++ linkedlist/doublelinkedlist.go
@@ -2,6 +2,24 @@ package linkedlist
 
 import "fmt"
 
+type DoubleLinkedList[T any] interface {
+	Head() *DoubleNode[T]
+	Tail() *DoubleNode[T]
+	// Append inserts new item at tail
+	Append(data T) *DoubleNode[T]
+	// Push appends new item at head
+	Push(data T) *DoubleNode[T]
+	Remove(node *DoubleNode[T]) *DoubleNode[T]
+	RemoveTail() *DoubleNode[T]
+	MoveToHead(node *DoubleNode[T])
+	Print()
+}
+
+func NewDoubleLinkedList[T any]() DoubleLinkedList[T] {
+	list := DoubleLinkedListImpl[T]{}
+	return &list
+}
+
 // DoubleNode represents a node in the linked list
 type DoubleNode[T any] struct {
 	Data T
@@ -9,58 +27,66 @@ type DoubleNode[T any] struct {
 	Next *DoubleNode[T]
 }
 
-// DoubleLinkedList represents the linked list
-type DoubleLinkedList[T any] struct {
-	Head *DoubleNode[T]
-	Tail *DoubleNode[T]
+// DoubleLinkedListImpl represents the linked list
+type DoubleLinkedListImpl[T any] struct {
+	head *DoubleNode[T]
+	tail *DoubleNode[T]
+}
+
+func (ll *DoubleLinkedListImpl[T]) Head() *DoubleNode[T] {
+	return ll.head
+}
+
+func (ll *DoubleLinkedListImpl[T]) Tail() *DoubleNode[T] {
+	return ll.tail
 }
 
 // Append adds a new node with the given data to the end of the list
-func (ll *DoubleLinkedList[T]) Append(data T) *DoubleNode[T] {
+func (ll *DoubleLinkedListImpl[T]) Append(data T) *DoubleNode[T] {
 	newNode := &DoubleNode[T]{Data: data, Prev: nil, Next: nil}
 
-	if ll.Head == nil {
-		ll.Head = newNode
-		ll.Tail = newNode
+	if ll.head == nil {
+		ll.head = newNode
+		ll.tail = newNode
 		return newNode
 	}
 
-	if ll.Tail != nil {
-		newNode.Prev = ll.Tail
-		ll.Tail.Next = newNode
-		ll.Tail = newNode
+	if ll.tail != nil {
+		newNode.Prev = ll.tail
+		ll.tail.Next = newNode
+		ll.tail = newNode
 	}
 
 	return newNode
 }
 
 // Push adds a new node with the given data to the head of the list
-func (ll *DoubleLinkedList[T]) Push(data T) *DoubleNode[T] {
+func (ll *DoubleLinkedListImpl[T]) Push(data T) *DoubleNode[T] {
 	newNode := &DoubleNode[T]{Data: data, Prev: nil, Next: nil}
 
-	if ll.Head == nil {
-		ll.Head = newNode
-		ll.Tail = newNode
+	if ll.head == nil {
+		ll.head = newNode
+		ll.tail = newNode
 		return newNode
 	}
 
-	newNode.Next = ll.Head
-	ll.Head.Prev = newNode
-	ll.Head = newNode
+	newNode.Next = ll.head
+	ll.head.Prev = newNode
+	ll.head = newNode
 
 	return newNode
 }
 
-func (ll *DoubleLinkedList[T]) Remove(node *DoubleNode[T]) *DoubleNode[T] {
+func (ll *DoubleLinkedListImpl[T]) Remove(node *DoubleNode[T]) *DoubleNode[T] {
 
 	switch {
-	case node == ll.Head:
-		ll.Head = ll.Head.Next
-		ll.Head.Prev = nil
+	case node == ll.head:
+		ll.head = ll.head.Next
+		ll.head.Prev = nil
 		return node
-	case node == ll.Tail:
-		ll.Tail = ll.Tail.Prev
-		ll.Tail.Next = nil
+	case node == ll.tail:
+		ll.tail = ll.tail.Prev
+		ll.tail.Next = nil
 		return node
 	default:
 		node.Prev.Next = node.Next
@@ -69,13 +95,13 @@ func (ll *DoubleLinkedList[T]) Remove(node *DoubleNode[T]) *DoubleNode[T] {
 	}
 }
 
-func (ll *DoubleLinkedList[T]) RemoveTail() *DoubleNode[T] {
-	return ll.Remove(ll.Tail)
+func (ll *DoubleLinkedListImpl[T]) RemoveTail() *DoubleNode[T] {
+	return ll.Remove(ll.tail)
 }
 
 // Print displays all the elements in the linked list
-func (ll *DoubleLinkedList[T]) Print() {
-	current := ll.Head
+func (ll *DoubleLinkedListImpl[T]) Print() {
+	current := ll.head
 	for current != nil {
 		fmt.Printf("%v -> ", current.Data)
 		current = current.Next
@@ -83,10 +109,10 @@ func (ll *DoubleLinkedList[T]) Print() {
 	fmt.Println("nil")
 }
 
-func (ll *DoubleLinkedList[T]) MoveToHead(node *DoubleNode[T]) {
+func (ll *DoubleLinkedListImpl[T]) MoveToHead(node *DoubleNode[T]) {
 	ll.Remove(node)
-	node.Next = ll.Head
-	ll.Head.Prev = node
-	ll.Head = node
-	ll.Head.Prev = nil
+	node.Next = ll.head
+	ll.head.Prev = node
+	ll.head = node
+	ll.head.Prev = nil
 }
--- linkedlist/doublelinkedlist_test.go
+++ linkedlist/doublelinkedlist_test.go
@@ -4,17 +4,17 @@ import (
 	"testing"
 )
 
-func createInitialList(data int) *DoubleLinkedList[int] {
-	list := DoubleLinkedList[int]{}
+func createInitialList(data int) DoubleLinkedList[int] {
+	list := NewDoubleLinkedList[int]()
 	list.Append(data)
-	return &list
+	return list
 }
-func createTwoItemList(data int, data2 int) *DoubleLinkedList[int] {
+func createTwoItemList(data int, data2 int) DoubleLinkedList[int] {
 	list := createInitialList(data)
 	list.Append(data2)
 	return list
 }
-func createThreeItemList(data int, data2 int, data3 int) *DoubleLinkedList[int] {
+func createThreeItemList(data int, data2 int, data3 int) DoubleLinkedList[int] {
 	list := createTwoItemList(data, data2)
 	list.Append(data3)
 	return list
@@ -28,41 +28,41 @@ func TestDoubleLinkedList_Append(t *testing.T) {
 
 	t.Run("initial insert", func(t *testing.T) {
 		list := createInitialList(data)
-		if list.Head != list.Tail {
+		if list.Head() != list.Tail() {
 			t.Errorf("expected head = tail")
 		}
-		if list.Head.Prev != nil {
+		if list.Head().Prev != nil {
 			t.Errorf("expected head.prev to be nil")
 		}
-		if list.Tail.Next != nil {
+		if list.Tail().Next != nil {
 			t.Errorf("expected tail.next to be nil")
 		}
-		if list.Head.Data != data {
-			t.Errorf("expected %d, got %d", data, list.Head.Data)
+		if list.Head().Data != data {
+			t.Errorf("expected %d, got %d", data, list.Head().Data)
 		}
 	})
 	t.Run("second insert", func(t *testing.T) {
 		list := createTwoItemList(data, data2)
-		if list.Head.Next != list.Tail {
+		if list.Head().Next != list.Tail() {
 			t.Errorf("expected head.next to be tail")
 		}
-		if list.Tail.Prev != list.Head {
+		if list.Tail().Prev != list.Head() {
 			t.Errorf("expected tail.prev to be head")
 		}
-		if list.Tail.Data != data2 {
-			t.Errorf("expected %d, got %d", data2, list.Tail.Data)
+		if list.Tail().Data != data2 {
+			t.Errorf("expected %d, got %d", data2, list.Tail().Data)
 		}
 	})
 	t.Run("third insert", func(t *testing.T) {
 		list := createThreeItemList(data, data2, data3)
-		if list.Head.Next == list.Tail {
+		if list.Head().Next == list.Tail() {
 			t.Errorf("expected head.next != tail")
 		}
-		if list.Tail.Prev == list.Head {
+		if list.Tail().Prev == list.Head() {
 			t.Errorf("expected tail.prev != head")
 		}
-		if list.Tail.Data != data3 {
-			t.Errorf("expected %d, got %d", data3, list.Tail.Data)
+		if list.Tail().Data != data3 {
+			t.Errorf("expected %d, got %d", data3, list.Tail().Data)
 		}
 	})
 }
@@ -70,48 +70,48 @@ func TestDoubleLinkedList_Append(t *testing.T) {
 func TestDoubleLinkedList_Remove(t *testing.T) {
 	t.Run("remove head", func(t *testing.T) {
 		list := createThreeItemList(data, data2, data3)
-		origHead := list.Head
-		origTail := list.Tail
-		newHead := list.Head.Next
+		origHead := list.Head()
+		origTail := list.Tail()
+		newHead := list.Head().Next
 		nodeRemoved := list.Remove(origHead)
 		if nodeRemoved != origHead {
 			t.Errorf("expected original head returned")
 		}
-		if list.Head != newHead {
-			t.Errorf("expected new head")
+		if list.Head() != newHead {
+			t.Errorf("expected new Head()")
 		}
-		if list.Tail != origTail {
+		if list.Tail() != origTail {
 			t.Errorf("expected tail unchanged")
 		}
-		if list.Head.Next != list.Tail {
+		if list.Head().Next != list.Tail() {
 			t.Errorf("expected head.next == tail")
 		}
-		if list.Tail.Prev != list.Head {
+		if list.Tail().Prev != list.Head() {
 			t.Errorf("expected tail.prev == head")
 		}
 		if nodeRemoved.Data != data {
 			t.Errorf("expected %d, got %d", data, nodeRemoved.Data)
 		}
 	})
-	t.Run("remove tail", func(t *testing.T) {
+	t.Run("remove Tail()", func(t *testing.T) {
 		list := createThreeItemList(data, data2, data3)
-		origHead := list.Head
-		origTail := list.Tail
-		newTail := list.Tail.Prev
+		origHead := list.Head()
+		origTail := list.Tail()
+		newTail := list.Tail().Prev
 		nodeRemoved := list.Remove(origTail)
 		if nodeRemoved != origTail {
 			t.Errorf("expected original tail returned")
 		}
-		if list.Tail != newTail {
+		if list.Tail() != newTail {
 			t.Errorf("expected new tail")
 		}
-		if list.Head != origHead {
-			t.Errorf("expected head unchanged")
+		if list.Head() != origHead {
+			t.Errorf("expected Head() unchanged")
 		}
-		if list.Head.Next != list.Tail {
+		if list.Head().Next != list.Tail() {
 			t.Errorf("expected head.next == tail")
 		}
-		if list.Tail.Prev != list.Head {
+		if list.Tail().Prev != list.Head() {
 			t.Errorf("expected tail.prev == head")
 		}
 		if nodeRemoved.Data != data3 {
@@ -120,23 +120,23 @@ func TestDoubleLinkedList_Remove(t *testing.T) {
 	})
 	t.Run("remove mid", func(t *testing.T) {
 		list := createThreeItemList(data, data2, data3)
-		origHead := list.Head
-		origTail := list.Tail
-		expectedRemoved := list.Head.Next
+		origHead := list.Head()
+		origTail := list.Tail()
+		expectedRemoved := list.Head().Next
 		nodeRemoved := list.Remove(expectedRemoved)
 		if nodeRemoved != expectedRemoved {
 			t.Errorf("expected original mid node returned")
 		}
-		if list.Tail != origTail {
+		if list.Tail() != origTail {
 			t.Errorf("expected tail unchanged")
 		}
-		if list.Head != origHead {
+		if list.Head() != origHead {
 			t.Errorf("expected head unchanged")
 		}
-		if list.Head.Next != list.Tail {
-			t.Errorf("expected head.next == tail")
+		if list.Head().Next != list.Tail() {
+			t.Errorf("expected Head().next == tail")
 		}
-		if list.Tail.Prev != list.Head {
+		if list.Tail().Prev != list.Head() {
 			t.Errorf("expected tail.prev == head")
 		}
 		if nodeRemoved.Data != data2 {
@@ -147,41 +147,41 @@ func TestDoubleLinkedList_Remove(t *testing.T) {
 
 func TestDoubleLinkedList_Push(t *testing.T) {
 	list := createInitialList(data)
-	origHead := list.Head
+	origHead := list.Head()
 	newHead := list.Push(data2)
-	if list.Head != newHead {
+	if list.Head() != newHead {
 		t.Errorf("expected new head")
 	}
-	if list.Head.Next != origHead {
+	if list.Head().Next != origHead {
 		t.Errorf("expected old head to be second")
 	}
-	if origHead.Prev != list.Head {
+	if origHead.Prev != list.Head() {
 		t.Errorf("expected old head.prev to be new head")
 	}
-	if list.Head.Data != data2 {
-		t.Errorf("expected %d, got %d", data2, list.Head.Data)
+	if list.Head().Data != data2 {
+		t.Errorf("expected %d, got %d", data2, list.Head().Data)
 	}
 }
 
 func TestDoubleLinkedList_RemoveTail(t *testing.T) {
 	list := createThreeItemList(data, data2, data3)
-	origHead := list.Head
-	origTail := list.Tail
-	newTail := list.Tail.Prev
+	origHead := list.Head()
+	origTail := list.Tail()
+	newTail := list.Tail().Prev
 	nodeRemoved := list.RemoveTail()
 	if nodeRemoved != origTail {
 		t.Errorf("expected original tail returned")
 	}
-	if list.Tail != newTail {
+	if list.Tail() != newTail {
 		t.Errorf("expected new tail")
 	}
-	if list.Head != origHead {
+	if list.Head() != origHead {
 		t.Errorf("expected head unchanged")
 	}
-	if list.Head.Next != list.Tail {
+	if list.Head().Next != list.Tail() {
 		t.Errorf("expected head.next == tail")
 	}
-	if list.Tail.Prev != list.Head {
+	if list.Tail().Prev != list.Head() {
 		t.Errorf("expected tail.prev == head")
 	}
 	if nodeRemoved.Data != data3 {
@@ -191,24 +191,24 @@ func TestDoubleLinkedList_RemoveTail(t *testing.T) {
 
 func TestDoubleLinkedList_MoveToHead(t *testing.T) {
 	list := createThreeItemList(data, data2, data3)
-	origHead := list.Head
-	origTail := list.Tail
-	nodeToMove := list.Head.Next
+	origHead := list.Head()
+	origTail := list.Tail()
+	nodeToMove := list.Head().Next
 	list.MoveToHead(nodeToMove)
-	if list.Head != nodeToMove {
+	if list.Head() != nodeToMove {
 		t.Errorf("expected new head")
 	}
-	if list.Tail != origTail {
+	if list.Tail() != origTail {
 		t.Errorf("expected same tail")
 	}
-	if list.Head.Next != origHead {
-		t.Errorf("expected head.next == tail")
+	if list.Head().Next != origHead {
+		t.Errorf("expected orighead.next == tail")
 	}
-	if list.Tail.Prev != list.Head {
-		t.Errorf("expected tail.prev == head")
+	if list.Tail().Prev != origHead {
+		t.Errorf("expected tail.prev == orighead")
 	}
-	if list.Head.Data != data2 {
-		t.Errorf("expected %d, got %d", data2, list.Head.Data)
+	if list.Head().Data != data2 {
+		t.Errorf("expected %d, got %d", data2, list.Head().Data)
 	}
 }
 
