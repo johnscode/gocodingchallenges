21283aed08fb23edd52d78bc3bbd921788f81020
John F <john@johnscode.com>
2024-08-15 20:35:34 -0500

    fix findallpalindromes

--- allpalindromes/allpalindromes.go
+++ allpalindromes/allpalindromes.go
@@ -1,17 +1,29 @@
 package allpalindromes
 
-import (
-	"gocoding/palindromecheck"
-	"gocoding/uniquecombos"
-)
+func expand(i int, j int, str string, palindromes map[string]string) map[string]string {
+	runes := []rune(str)
+	for i >= 0 && j < len(str) && runes[i] == runes[j] {
+		pal := string(runes[i : j+1])
+		palindromes[pal] = pal
+		i -= 1
+		j += 1
+	}
+	return palindromes
+}
 
 func FindAllPalindromes(str string) []string {
-	allPalindromes := []string{}
-	uniqueStrings := uniquecombos.FindUniqueCombinations(str)
-	for _, uniqueString := range uniqueStrings {
-		if palindromecheck.PalindromeCheck(uniqueString) {
-			allPalindromes = append(allPalindromes, uniqueString)
-		}
+	palindromes := map[string]string{}
+
+	for i := 0; i < len(str); i++ {
+		palindromes = expand(i, i, str, palindromes)
 	}
-	return allPalindromes
+	for i := 0; i < len(str)-1; i++ {
+		palindromes = expand(i, i+1, str, palindromes)
+	}
+	//fmt.Printf("palindromes: %+v", palindromes)
+	res := []string{}
+	for _, v := range palindromes {
+		res = append(res, v)
+	}
+	return res
 }
--- allpalindromes/allpalindromes_test.go
+++ allpalindromes/allpalindromes_test.go
@@ -1,6 +1,7 @@
 package allpalindromes
 
 import (
+	"fmt"
 	"reflect"
 	"slices"
 	"testing"
@@ -15,15 +16,16 @@ func TestFindAllPalindromes(t *testing.T) {
 		{"", []string{}},
 		{"a", []string{"a"}},
 		{"ab", []string{"a", "b"}},
-		{"aba", []string{"a", "aa", "aba", "b"}},
+		{"aba", []string{"a", "aba", "b"}},
 		{"aab", []string{"a", "aa", "b"}},
-		{"abcba", []string{"a", "aa", "aba", "abba", "abcba", "aca", "b", "bb", "bcb", "c"}},
+		{"abcba", []string{"a", "abcba", "b", "bcb", "c"}},
 	}
 
 	for _, tc := range testCases {
 		results := FindAllPalindromes(tc.input)
 		// sort result to match expected order
 		slices.Sort(results)
+		fmt.Printf("results %+v\n", results)
 		if !reflect.DeepEqual(results, tc.expected) {
 			t.Errorf("findUniqueCombinations(%q) = %v; expected %v", tc.input, results, tc.expected)
 		}
